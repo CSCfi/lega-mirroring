c330c7ad2557d947a46fe4f034e569b8a8f99ca7
teemukataja <kataja.teemu@gmail.com>
2017-06-06 10:51:31 +0300

    Broke large function into three smaller functions and fixed logging date format

--- decrypt_request.py
+++ decrypt_request.py
@@ -8,10 +8,10 @@ import logging
 #host_url = 'http://86.50.169.120:9090/file/'
 logging.basicConfig(filename='decrypt_log.log',
                     format='%(asctime)s %(message)s',
-                    datefmt='%d-%M-%Y %I:%M:%S',
+                    datefmt='%d-%m-%Y %I:%M:%S',
                     level=logging.INFO)
 
-
+'''
 def decrypt(host_url, file_path):
     """ This function decrypts file type file.bam.cip
     and saves the decrypted file to file.txt """
@@ -20,6 +20,7 @@ def decrypt(host_url, file_path):
               'sourceKey': 'aeskey',
               'destinationFormat': 'plain'}
     r = requests.get(host_url, params, stream=True)
+    print(r)  # testi
     if r:
         with open('decrypted_file.txt', 'wb+') as f:
             for chunk in r.iter_content(chunk_size=1024):
@@ -32,6 +33,42 @@ def decrypt(host_url, file_path):
                  ' http-request: ' + host_url +
                  ' path: ' + file_path)
     return
+'''
+
+def decrypt(host_url, file_path):
+    """ This function returns a stream of decrypted data """
+    params = {'filePath': file_path,
+              'sourceFormat': 'aes128',
+              'sourceKey': 'aeskey',
+              'destinationFormat': 'plain'}
+    r = requests.get(host_url, params, stream=True)
+    log_event(r, host_url, file_path)
+    return r
+
+
+def write_to_file(feed):
+    """ This function handles a stream of data and writes
+    it to file """
+    with open('decrypted_file.txt', 'wb+') as f:
+        for chunk in feed.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+    return
+
+    
+def log_event(event, host, path):
+    """ This function logs successes and failures to file """
+    if event:
+        logging.info(' OK: http-request: ' + host +
+                 ' path: ' + path)
+    else:
+        logging.info(' ERROR: Check that url and path are correct -'
+                 ' http-request: ' + host +
+                 ' path: ' + path)
+    return
+
+
+
 
 
 '''*************************************************************'''
@@ -42,7 +79,7 @@ def decrypt(host_url, file_path):
 def main(arguments=None):
     """ This function runs the script with given arguments (host and path) """
     args = parse_arguments(arguments)
-    decrypt(args.host, args.path)
+    write_to_file(decrypt(args.host, args.path))
     return
 
 
