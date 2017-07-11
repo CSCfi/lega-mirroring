b8db1945fde80a252f166fbee81ac92065d33216
teemukataja <kataja.teemu@gmail.com>
2017-07-11 15:20:42 +0300

    changed dest to be read from config.ini instead of cmdline param

--- lega_mirroring/scripts/move.py
+++ lega_mirroring/scripts/move.py
@@ -3,11 +3,23 @@ import shutil
 import argparse
 import sys
 import logging
+from configparser import ConfigParser
+from collections import namedtuple
 
 logging.basicConfig(filename='move_log.log',
                     format='%(asctime)s %(message)s',
                     datefmt='%d-%m-%Y %H:%M:%S',
                     level=logging.INFO)
+
+
+def get_conf(path_to_config):
+    """ This function reads configuration variables from an external file
+    and returns the configuration variables as a class object """
+    config = ConfigParser()
+    config.read(path_to_config)
+    conf = {'end_storage': config.getint('workspaces', 'end_storage')}
+    conf_named = namedtuple("Config", conf.keys())(*conf.values())
+    return conf_named
 
 
 def move(file, dest):
@@ -30,18 +42,24 @@ def log_event(file, dest):
 
 
 def main(arguments=None):
-    """ This function runs the script with given arguments (file and dest) """
+    """ This function moves given file to end storage location """
     args = parse_arguments(arguments)
-    move(args.file, args.dest)
+    conf = args.path_to_config
+    config = get_conf(conf)
+    move(args.file, config.end_storage)
     return
 
 
 def parse_arguments(arguments):
     """ This function returns the parsed arguments
     file(which is a file) and dest(which is a directory) """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('file')
-    parser.add_argument('dest')
+    parser = argparse.ArgumentParser(description='Move file to'
+                                     ' predetermined location set in'
+                                     ' config.ini')
+    parser.add_argument('file',
+                        help='path to file to be moved')
+    parser.add_argument('path_to_config',
+                        help='path to configuration file.')
     return parser.parse_args(arguments)
 
 
