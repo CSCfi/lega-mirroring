49c91a00488373ae4c7af1ff457909a9841723ea
teemukataja <kataja.teemu@gmail.com>
2017-07-25 09:53:47 +0300

    removed obsolete comments

--- lega_mirroring/workflows/TransferTracking.py
+++ lega_mirroring/workflows/TransferTracking.py
@@ -8,13 +8,6 @@ class CheckFilesInDirectory(luigi.Task):
     branch = luigi.Parameter()
     config = luigi.Parameter()
 
-    '''
-    Has no requires() method
-      * independent script
-    Has no output() method
-      * cf_dir utilizes it's own db-tracking
-    '''
-
     def run(self):
         ''' This function starts the file transfer checking process
         as described in lega_mirroring/scripts/monitor.py '''
