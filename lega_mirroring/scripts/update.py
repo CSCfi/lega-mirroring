1b7d81e7422bd6dc3d84ff6ec4a1a8abad53a0a5
teemukataja <kataja.teemu@gmail.com>
2017-08-07 10:18:29 +0300

    added new script to update file details in db after file processing has finished

---
+++ lega_mirroring/scripts/update.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3.4
+import MySQLdb
+import sys
+import argparse
+import logging
+import os
+import ntpath
+from configparser import ConfigParser
+from collections import namedtuple
+
+logging.basicConfig(filename='update_log.log',
+                    format='%(asctime)s %(message)s',
+                    datefmt='%d-%M-%Y %H:%M:%S',
+                    level=logging.INFO)
+
+
+def get_conf(path_to_config):
+    """ 
+    This function reads configuration variables from an external file
+    and returns the configuration variables as a class object 
+    
+    :path_to_config: full path to config.ini (or just config.ini if
+                     cwd: lega-mirroring)
+    """
+    config = ConfigParser()
+    config.read(path_to_config)
+    conf = {'host': config.get('database', 'host'),
+            'user': config.get('database', 'user'),
+            'passwd': config.get('database', 'passwd'),
+            'db': config.get('database', 'db'),
+            'path_archive': config.get('workspaces', 'end_storage'),
+            'path_gridftp': config.get('workspaces', 'receiving')}
+    conf_named = namedtuple("Config", conf.keys())(*conf.values())
+    return conf_named
+
+    
+def db_init(hostname, username, password, database):
+    """ 
+    This function initializes database connection and returns a connection
+    object that will be used as an executale cursor object
+    
+    :hostname: address of mysql server
+    :username: username to log in to mysql server
+    :password: password associated with :username: to log in to mysql server
+    :database: database to be worked on
+    """
+    db = MySQLdb.connect(host=hostname,
+                         user=username,
+                         passwd=password,
+                         db=database)
+    return db
+
+
+def db_update_file(db, filename, path_archive, path_gridftp):
+    """
+    This function updates file path and status to database
+    
+    :db: database connection object
+    :path_file: path to file
+    :path_archive: path to end storage
+    :path_gridftp: path to receivings directory
+    """
+    oldfile = os.path.join(path_gridftp, filename)
+    newfile = os.path.join(path_archive, filename)
+    params = ['available', newfile, oldfile]
+    cur = db.cursor()
+    cur.execute('UPDATE file '
+                'SET status=%s, file_name=%s '
+                'WHERE file_name=%s;',
+                params)
+    db.commit()
+    print(params)
+    return
+
+
+'''*************************************************************'''
+#                         cmd-executable                          #
+'''*************************************************************'''
+
+
+def main(arguments=None):
+    """ 
+    This function runs the script
+    
+    :arguments: contains parsed command line parameters
+    """
+    args = parse_arguments(arguments)
+    conf = args.config
+    # Get configuration values from external file
+    config = get_conf(conf)
+    # Establish database connection
+    db = db_init(config.host,
+                 config.user,
+                 config.passwd,
+                 config.db)
+    db_update_file(db, args.path, config.path_archive, config.path_gridftp)
+    return
+
+
+def parse_arguments(arguments):
+    """ 
+    This function parses command line inputs and returns them for main()
+    
+    :path: path to file that\'s details are updated 
+    :config: full path to config.ini (or just config.ini if
+                     cwd: lega-mirroring)
+    """
+    parser = argparse.ArgumentParser(description='Update file status '
+                                     'and path')
+    parser.add_argument('path',
+                        help='base filename')
+    parser.add_argument('config',
+                        help='path to configuration file.')
+    return parser.parse_args(arguments)
+
+
+if __name__ == '__main__':
+    RETVAL = main()
+    sys.exit(RETVAL)
