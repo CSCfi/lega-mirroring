10fbdd85078647187d01ebd9d394f6b60ea203d4
teemukataja <kataja.teemu@gmail.com>
2017-09-06 15:42:27 +0300

    removed old directory

--- tests/test_md5hasher.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from lega_mirroring import md5hasher_dir
-
-
-def test_md5():
-    md5sum = md5hasher_dir.md5('tests/data/text-file.txt')
-    assert md5sum == 'd881b69b0aca33bcf5dbc7dc5c448cc2'
